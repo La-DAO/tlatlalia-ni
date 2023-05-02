8a1fe46c18f7cf93632df786e304331eb3f35fc8
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-05-02 21:18:33 +0100

    feat: added redstone facet and associated contracts

--- src/facets/RedstoneFacet.sol
+++ src/facets/RedstoneFacet.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.17;
 
-contract RedstoneFacet {
-    function pushRedstonePrice() external {}
+import {MainConsumerBase} from "../external/redstone/MainConsumerBase.sol";
+
+contract RedstoneFacet is MainConsumerBase {
+    function pushRedstonePrice() external view returns (uint256) {
+        uint256 mxnUsdPrice = getOracleNumericValueFromTxMsg(bytes32("MXN"));
+        return mxnUsdPrice;
+    }
 }
