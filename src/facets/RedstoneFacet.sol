c2d87f5a658a232972e83dc3ea6c67ea1558368a
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-07-16 17:49:30 -0700

    chore: define store and get method redstone oracle

--- src/facets/RedstoneFacet.sol
+++ src/facets/RedstoneFacet.sol
@@ -3,8 +3,27 @@ pragma solidity 0.8.17;
 
 import {MainConsumerBase} from "../external/redstone/MainConsumerBase.sol";
 
+struct RedstoneFacetStorage {
+    int256 storedLatestPrice;
+    uint256 decimals;
+    uint256 lastTimestamp;
+}
+
 contract RedstoneFacet is MainConsumerBase {
-    function getLatestRedstonePrice() public view returns (uint256) {
-        return getOracleNumericValueFromTxMsg(bytes32("ETH"));
+    bytes32 private constant REDSTONE_TICKER = bytes32("MXN");
+
+    RedstoneFacetStorage internal _SRedstoneFacet;
+
+    constructor() {
+        _SRedstoneFacet.decimals = 8;
+    }
+
+    function get_MXNUSD_Price_Redstone() public view returns (int256) {
+        return _SRedstoneFacet.storedLatestPrice;
+    }
+
+    function store_MXNUSD_Price_Redstone() public {
+        _SRedstoneFacet.storedLatestPrice = int256(getOracleNumericValueFromTxMsg(REDSTONE_TICKER));
+        _SRedstoneFacet.lastTimestamp = block.timestamp;
     }
 }
