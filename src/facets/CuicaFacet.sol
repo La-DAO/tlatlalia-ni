e4d07769bf726a81113785f38b06c3512ab943f9
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-08-27 22:01:34 -0700

    chore: update cuicafacet to work with bulletin signatures

--- src/facets/CuicaFacet.sol
+++ src/facets/CuicaFacet.sol
@@ -2,86 +2,174 @@
 pragma solidity 0.8.17;
 
 import {IAggregatorV3} from "../interfaces/chainlink/IAggregatorV3.sol";
-import {AppStorage, RoundData, CuicaFacetStorage, OracleFacetStorage} from "../libraries/AppStorage.sol";
-// import {RedstoneFacet} from "./RedstoneFacet.sol";
-// import {PythFacet} from "./PythFacet.sol";
-// import {ChainlinkFacet} from "./ChainlinkFacet.sol";
+import {
+  AppStorage, RoundData, CuicaFacetStorage, OracleFacetStorage
+} from "../libraries/AppStorage.sol";
+import {LibDiamond} from "../libraries/LibDiamond.sol";
+import {IConnext} from "../interfaces/connext/IConnext.sol";
+import {BulletinSigning} from "../BulletinSigning.sol";
 
-import {console} from "forge-std/console.sol";
+contract CuicaFacet is IAggregatorV3, BulletinSigning, AppStorage {
+  /// Events
+  event SetConnext(address newConnext);
 
-contract CuicaFacet is IAggregatorV3, AppStorage {
-    /// Custom Errors
-    error CuicaFacet_aggregateAndPublishRound_notWithinTimeLimit();
+  /// Custom Errors
+  error CuicaFacet__setters_invalidInput();
+  error CuicaFacet_aggregateAndPublishRound_notWithinTimeLimit();
+  error CuicaFacet__tlatlaliaNi_wrongSizeArrays();
 
-    uint256 constant internal WORKING_TIME_GAP_LIMIT = 5 minutes;
+  uint256 internal constant WORKING_TIME_GAP_LIMIT = 5 minutes;
 
-    function decimals() external pure returns (uint8) {
-        return 8;
+  function decimals() external pure returns (uint8) {
+    return 8;
+  }
+
+  function description() external pure returns (string memory) {
+    return "MXN / USD";
+  }
+
+  function version() external pure returns (string memory) {
+    return VERSION;
+  }
+
+  function latestAnswer() external view returns (int256) {
+    (, int256 answer,,,) = latestRoundData();
+    return answer;
+  }
+
+  function latestRound() public view returns (uint80) {
+    CuicaFacetStorage storage cs = accessCuicaStorage();
+    return cs.lastRound;
+  }
+
+  function latestRoundData()
+    public
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    )
+  {
+    uint80 lastRound = latestRound();
+    CuicaFacetStorage storage cs = accessCuicaStorage();
+    if (lastRound == 0) {
+      return (0, 0, 0, 0, 0);
+    } else {
+      return (
+        lastRound,
+        cs.roundInfo[lastRound].answer,
+        cs.roundInfo[lastRound].startedAt,
+        cs.roundInfo[lastRound].updatedAt,
+        lastRound
+      );
+    }
+  }
+
+  function aggregateAndPublishRound() external {
+    OracleFacetStorage storage osr = accessOracleStorage(REDSTONE_STORAGE_POSITION);
+    OracleFacetStorage storage osp = accessOracleStorage(PYTH_STORAGE_POSITION);
+    OracleFacetStorage storage osc = accessOracleStorage(CHAINLINK_STORAGE_POSITION);
+
+    uint256 timestampLimit = block.timestamp - WORKING_TIME_GAP_LIMIT;
+    if (
+      osr.workingTimestamp < timestampLimit || osp.workingTimestamp < timestampLimit
+        || osc.workingTimestamp < timestampLimit
+    ) {
+      revert CuicaFacet_aggregateAndPublishRound_notWithinTimeLimit();
     }
 
-    function description() external pure returns (string memory) {
-        return "MXN / USD";
+    int256 sum = osr.storedLatestPrice + osp.storedLatestPrice + osc.storedLatestPrice;
+    uint256 average = uint256(sum) / 3;
+
+    CuicaFacetStorage storage cs = accessCuicaStorage();
+
+    uint80 thisRoundId = cs.lastRound + 1;
+    cs.lastRound += 1;
+
+    RoundData memory newRound = RoundData({
+      roundId: thisRoundId,
+      answer: int256(average),
+      startedAt: block.timestamp,
+      updatedAt: block.timestamp,
+      answeredInRound: thisRoundId
+    });
+    cs.roundInfo[thisRoundId] = newRound;
+  }
+
+  /**
+   * @notice Sing the price to all domain bulletins.
+   */
+  function tlatlaliaNi(
+    uint32 domain,
+    address destination,
+    uint256 cost,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  )
+    external
+    payable
+    returns (bool)
+  {
+    CuicaFacetStorage storage cs = accessCuicaStorage();
+    RoundData memory lastRound = cs.roundInfo[cs.lastRound];
+
+    bytes memory bulletinData = abi.encode(lastRound, v, r, s);
+
+    IConnext(cs.connext).xcall{value: cost}(
+      // _destination: Domain ID of the destination chain
+      domain,
+      // _to: address of the target contract
+      destination,
+      // _asset: address of the token contract
+      address(0),
+      // _delegate: address that can revert or forceLocal on destination
+      destination,
+      // _amount: amount of tokens to transfer
+      0,
+      // _slippage: can be anything between 0-10000 becaus
+      // the maximum amount of slippage the user will accept in BPS, 30 == 0.3%
+      0,
+      // _callData: the encoded calldata to send
+      bulletinData
+    );
+
+    return true;
+  }
+
+  function setConnext(address connext_) external {
+    if (connext_ == address(0)) {
+      revert CuicaFacet__setters_invalidInput();
     }
+    LibDiamond.enforceIsContractOwner();
 
-    function version() external pure returns (uint256) {
-        return 1;
-    }
+    CuicaFacetStorage storage cs = accessCuicaStorage();
+    cs.connext = connext_;
 
-    function latestAnswer() external view returns (int256) {
-        (, int256 answer,,,) = latestRoundData();
-        return answer;
-    }
+    emit SetConnext(connext_);
+  }
 
-    function latestRound() public view returns (uint80) {
-        CuicaFacetStorage storage cs = accessCuicaStorage();
-        return cs.lastRound;
-    }
+  /**
+   * @notice Returns the struct hash for `latestRoundData`.
+   */
+  function getStructHashLastRoundData() public view returns (bytes32) {
+    CuicaFacetStorage storage cs = accessCuicaStorage();
+    uint80 lastRoundId = cs.lastRound;
+    RoundData memory lastRound = cs.roundInfo[lastRoundId];
+    return getStructHashRoundData(lastRound);
+  }
 
-    function latestRoundData()
-        public
-        view
-        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
-    {
-        uint80 lastRound = latestRound();
-        CuicaFacetStorage storage cs = accessCuicaStorage();
-        if (lastRound == 0) {
-            return (0, 0, 0, 0, 0);
-        } else {
-            return (
-                lastRound,
-                cs.roundInfo[lastRound].answer,
-                cs.roundInfo[lastRound].startedAt,
-                cs.roundInfo[lastRound].updatedAt,
-                lastRound
-            );
-        }
-    }
-
-    function aggregateAndPublishRound() external {
-        OracleFacetStorage storage osr = accessOracleStorage(REDSTONE_STORAGE_POSITION);
-        OracleFacetStorage storage osp = accessOracleStorage(PYTH_STORAGE_POSITION);
-        OracleFacetStorage storage osc = accessOracleStorage(CHAINLINK_STORAGE_POSITION);
-
-        uint256 timestampLimit = block.timestamp - WORKING_TIME_GAP_LIMIT;
-        if (osr.workingTimestamp < timestampLimit || osp.workingTimestamp < timestampLimit || osc.workingTimestamp < timestampLimit) {
-            revert CuicaFacet_aggregateAndPublishRound_notWithinTimeLimit();
-        }
-
-        int256 sum = osr.storedLatestPrice + osp.storedLatestPrice + osc.storedLatestPrice;
-        uint256 average = uint256(sum) / 3;
-
-        CuicaFacetStorage storage cs = accessCuicaStorage();
-
-        uint80 thisRoundId = cs.lastRound + 1;
-        cs.lastRound += 1;
-
-        RoundData memory newRound = RoundData({
-            roundId: thisRoundId,
-            answer: int256(average),
-            startedAt: block.timestamp,
-            updatedAt: block.timestamp,
-            answeredInRound: thisRoundId
-        });
-        cs.roundInfo[thisRoundId] = newRound;
-    }
+  /**
+   * @dev Returns the domainSeparator for "CuicaFacet", reading chain Id from blockchain.
+   */
+  function _getDomainSeparator() internal view override returns (bytes32) {
+    return keccak256(
+      abi.encode(
+        TYPEHASH, NAMEHASH, VERSIONHASH, address(this), keccak256(abi.encode(block.chainid))
+      )
+    );
+  }
 }
