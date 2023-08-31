2df748bb6d19d7461584031017faa0a1fc71fecd
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-08-31 13:02:43 -0700

    feat: deployment script for full diamond

---
+++ scripts/hardhat/01_deployDiamondComplete.js
@@ -0,0 +1,14 @@
+const { deployDiamondComplete } = require("./deployDiamondComplete")
+
+const main = async () => {
+  console.log("\n\n 📡 Deploying...\n")
+  const diamondAddress = await deployDiamondComplete()
+  console.log("Diamond " + ": deployed at", diamondAddress)
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(`\n${error}\n`);
+    process.exit(1);
+  });
