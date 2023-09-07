8d876095ebe58f87ed8515f7989799319285381e
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-09-07 15:52:40 -0700

    feat: add duration formatter function

--- scripts/utilsCuica.js
+++ scripts/utilsCuica.js
@@ -31,6 +31,38 @@ function getEnvWSigner(provider) {
   return new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 }
 
+function formatDuration(milliseconds) {
+  const seconds = Math.floor(milliseconds / 1000);
+  const days = Math.floor(seconds / 86400);
+  const hours = Math.floor((seconds % 86400) / 3600);
+  const minutes = Math.floor(((seconds % 86400) % 3600) / 60);
+  const remainingSeconds = ((seconds % 86400) % 3600) % 60;
+
+  const parts = [];
+
+  if (days > 0) {
+    parts.push(`${days} day${days === 1 ? '' : 's'}`);
+  }
+
+  if (hours > 0) {
+    parts.push(`${hours} hour${hours === 1 ? '' : 's'}`);
+  }
+
+  if (minutes > 0) {
+    parts.push(`${minutes} minute${minutes === 1 ? '' : 's'}`);
+  }
+
+  if (remainingSeconds > 0) {
+    parts.push(`${remainingSeconds} second${remainingSeconds === 1 ? '' : 's'}`);
+  }
+
+  if (parts.length === 0) {
+    return '0 seconds';
+  }
+
+  return parts.join(', ');
+}
+
 function getTime() {
   return new Date()
 }
@@ -128,5 +160,6 @@ module.exports = {
   getChainProvider,
   getVoidSigner,
   getEnvWSigner,
+  formatDuration,
   logNewLine
 }
