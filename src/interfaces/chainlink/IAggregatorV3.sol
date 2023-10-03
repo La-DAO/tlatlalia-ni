8fe8eed484f110b73b1a028e7290f5b1e038901d
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-10-03 09:32:59 -0700

    docs: add to iaggregatorv3 interface

--- src/interfaces/chainlink/IAggregatorV3.sol
+++ src/interfaces/chainlink/IAggregatorV3.sol
@@ -1,13 +1,31 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.17;
 
+/**
+ * @title IAggregatorV3
+ *
+ * @notice Defines the common interfaces for Chainlink price feeds
+ */
+
 interface IAggregatorV3 {
+  /**
+   * @notice Returns the decimals of price feed
+   */
   function decimals() external view returns (uint8);
 
+  /**
+   * @notice Returns the description of this price feed
+   */
   function description() external view returns (string memory);
 
+  /**
+   * @notice Returns the version of this price feed
+   */
   function version() external view returns (string memory);
 
+  /**
+   * @notice Returns the last RoundData details of this price feed
+   */
   function latestRoundData()
     external
     view
