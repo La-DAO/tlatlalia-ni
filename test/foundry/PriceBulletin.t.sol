c010f95a4ac54520fb434616e07b55ab08787d96
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-10-04 20:38:35 -0700

    test: add more test coverage for price bulleting foundry unit tests

--- test/foundry/PriceBulletin.t.sol
+++ test/foundry/PriceBulletin.t.sol
@@ -2,19 +2,33 @@
 pragma solidity 0.8.17;
 
 import {Test, console} from "forge-std/Test.sol";
-import {PriceBulletin, RoundData} from "../../src/PriceBulletin.sol";
+import {PriceBulletin, RoundData, IERC20} from "../../src/PriceBulletin.sol";
 import {ERC1967Proxy} from "../../src/libraries/openzeppelin/ERC1967Proxy.sol";
 
 contract PriceBulletinUnitTests is Test {
-  uint256 constant ALICE_PK = 0xA;
-  address public OWNER = vm.addr(ALICE_PK);
+  // PriceBulletin Expected events
+  event BulletinUpdated(uint80 indexed rounId, int256 answer);
+  event FailedBulletinUpdate(string err);
+  event EarnedReward(address indexed owner, address indexed token, uint256 amount);
+  event ClaimedReward(address indexed owner, address indexed token, uint256 amount);
+  event SetAuthorizedPublisher(address publisher, bool status);
+  event SetReward(address token, uint256 amount);
 
-  uint256 constant BOB_PK = 0xB;
-  address public PUBLISHER = vm.addr(BOB_PK);
+  uint256 constant OWNER_PK = 0xA;
+  address public OWNER = vm.addr(OWNER_PK);
+
+  uint256 constant PUBLISHER_PK = 0xB;
+  address public PUBLISHER = vm.addr(PUBLISHER_PK);
+
+  uint256 constant CHARLY_PK = 0xC;
+  address public USER = vm.addr(CHARLY_PK);
 
   PriceBulletin public bulletin;
 
-  RoundData public fakeRound = RoundData(40, 5820537, 1695414655, 1695414655, 40);
+  RoundData public testRound = RoundData(40, 5820537, 1695414655, 1695414655, 40);
+
+  address public rewardToken = 0xa411c9Aa00E020e4f88Bc19996d29c5B7ADB4ACf;
+  uint256 public rewardAmount = 250e18;
 
   function setUp() public {
     vm.label(OWNER, "owner");
@@ -68,6 +82,135 @@ contract PriceBulletinUnitTests is Test {
     assertEq(round, 0);
   }
 
+  function test_publisherSignAndUpdatesBulletin() public {
+    test_ReturnLatestRoundDataBeforeEstablished();
+    (uint8 v, bytes32 r, bytes32 s) = sign_round(testRound, PUBLISHER_PK);
+
+    vm.expectEmit(true, false, false, true);
+    emit BulletinUpdated(testRound.roundId, testRound.answer);
+    publish_round(USER, testRound, v, r, s);
+
+    (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+      bulletin.latestRoundData();
+    assertEq(roundId, testRound.roundId);
+    assertEq(answer, testRound.answer);
+    assertEq(startedAt, testRound.startedAt);
+    assertEq(updatedAt, testRound.updatedAt);
+    assertEq(answeredInRound, testRound.answeredInRound);
+  }
+
+  function test_NonPublisherSignsAndAttemptsUpdatesBulletin() public {
+    test_ReturnLatestRoundDataBeforeEstablished();
+    (uint8 v, bytes32 r, bytes32 s) = sign_round(testRound, OWNER_PK);
+
+    vm.expectEmit(true, false, false, true);
+    emit FailedBulletinUpdate("Bad publisher!");
+    publish_round(USER, testRound, v, r, s);
+
+    (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+      bulletin.latestRoundData();
+    assertEq(roundId, 0);
+    assertEq(answer, 0);
+    assertEq(startedAt, 0);
+    assertEq(updatedAt, 0);
+    assertEq(answeredInRound, 0);
+  }
+
+  function test_SameRoundIdAttemptsUpdatesBulletin(RoundData memory anotherRound) public {
+    (uint8 v, bytes32 r, bytes32 s) = sign_round(testRound, PUBLISHER_PK);
+    publish_round(USER, testRound, v, r, s);
+
+    anotherRound.roundId = testRound.roundId;
+    (v, r, s) = sign_round(anotherRound, PUBLISHER_PK);
+
+    vm.expectEmit(true, false, false, true);
+    emit FailedBulletinUpdate("Bad RoundId!");
+    publish_round(USER, anotherRound, v, r, s);
+
+    (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) =
+      bulletin.latestRoundData();
+
+    assertEq(roundId, testRound.roundId);
+    assertEq(answer, testRound.answer);
+    assertEq(startedAt, testRound.startedAt);
+    assertEq(updatedAt, testRound.updatedAt);
+    assertEq(answeredInRound, testRound.answeredInRound);
+  }
+
+  function test_updateBulletinNeverReverts(
+    RoundData memory round,
+    uint128 privateKey,
+    address user
+  )
+    public
+  {
+    vm.assume(privateKey > 0);
+    (uint8 v, bytes32 r, bytes32 s) = sign_round(round, privateKey);
+    publish_round(user, round, v, r, s);
+  }
+
+  function test_setReward(address token, uint128 amount) public {
+    if (token == address(0) || amount == 0) {
+      vm.expectRevert();
+      set_reward(token, amount);
+    } else {
+      vm.expectEmit(true, true, false, true);
+      emit SetReward(token, uint256(amount));
+      set_reward(token, amount);
+    }
+  }
+
+  function testFail_setRewardNotOwner(address notAnOwner) public {
+    vm.assume(notAnOwner != OWNER);
+
+    vm.prank(notAnOwner);
+    bulletin.setReward(IERC20(rewardToken), rewardAmount);
+  }
+
+  function test_UpdatesBulletinWithRewardLog() public {
+    set_reward(rewardToken, rewardAmount);
+    (uint8 v, bytes32 r, bytes32 s) = sign_round(testRound, PUBLISHER_PK);
+
+    bytes memory callData = abi.encode(testRound, v, r, s);
+    vm.expectEmit(true, true, false, true);
+    emit EarnedReward(USER, rewardToken, rewardAmount);
+    vm.prank(USER);
+    bulletin.updateBulletinWithRewardLog(callData);
+
+    uint256 accumulatedRewards = bulletin.getRewards(USER, rewardToken);
+    assertEq(accumulatedRewards, rewardAmount);
+  }
+
+  function test_UpdatesBulletinWithRewardLogButNoReward() public {
+    (uint8 v, bytes32 r, bytes32 s) = sign_round(testRound, PUBLISHER_PK);
+
+    bytes memory callData = abi.encode(testRound, v, r, s);
+    vm.expectRevert(
+      PriceBulletin.PriceBulletin__checkRewardTokenAndAmount_noRewardTokenOrAmount.selector
+    );
+    vm.prank(USER);
+    bulletin.updateBulletinWithRewardLog(callData);
+
+    uint256 accumulatedRewards = bulletin.getRewards(USER, rewardToken);
+    assertEq(accumulatedRewards, 0);
+  }
+
+  function test_UpdateBulletinWithRewardLogWrongCalldata(
+    RoundData memory round,
+    uint128 randoPK
+  )
+    public
+  {
+    vm.assume(randoPK >0 && randoPK != PUBLISHER_PK);
+    set_reward(rewardToken, rewardAmount);
+    (uint8 v, bytes32 r, bytes32 s) = sign_round(round, randoPK);
+    bytes memory callData = abi.encode(round, v, r, s);
+    vm.prank(USER);
+    bulletin.updateBulletinWithRewardLog(callData);
+    uint256 accumulatedRewards = bulletin.getRewards(USER, rewardToken);
+    assertEq(accumulatedRewards, 0);
+  }
+
   /// Util functions
 
   function equal_strings(string memory str1, string memory str2) internal pure returns (bool) {
@@ -99,5 +242,10 @@ contract PriceBulletinUnitTests is Test {
     bytes memory callData = abi.encode(round, v, r, s);
     vm.prank(caller);
     bulletin.updateBulletin(callData);
+  }
+
+  function set_reward(address token, uint256 reward) internal {
+    vm.prank(OWNER);
+    bulletin.setReward(IERC20(token), reward);
   }
 }
