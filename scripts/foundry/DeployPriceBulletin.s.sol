4bb7b8186e0522d081eb3ded631bc0565adfd490
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-10-05 19:19:22 -0700

    chore: script to deploy price bulletin as upgradeable proxy

---
+++ scripts/foundry/DeployPriceBulletin.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import {console} from "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+import {PriceBulletin, RoundData, IERC20} from "../../src/PriceBulletin.sol";
+import {ERC1967Proxy} from "../../src/libraries/openzeppelin/ERC1967Proxy.sol";
+
+contract DeployPriceBulletin is Script {
+  address constant PUBLISHER = 0x2a895CF57ef296d1C63FE082053238D99D749774;
+
+  /**
+   * @dev Run using shell command:
+   * $forge script --rpc-url $<RPC_CHAIN> --private-key $<PRIVATE_KEY> --slow --verify --etherscan-api-key $<etherscan_key> --broadcast scripts/foundry/DeployPriceBulletin
+   */
+  function run() public {
+    vm.startBroadcast();
+    address implementation = address(new PriceBulletin());
+    console.log("Deployed implementation {PriceBulletin}:", implementation);
+    bytes memory _data = abi.encodeWithSelector(PriceBulletin.initialize.selector);
+    address proxyBulletin = address(new ERC1967Proxy(implementation, _data));
+    console.log("Proxy for {PriceBulletin} deployed:", proxyBulletin);
+    PriceBulletin(proxyBulletin).setAuthorizedPublisher(PUBLISHER, true);
+    vm.stopBroadcast();
+  }
+}
