a1a54d434c4985d4e43e94b75e440f9ed0fecee2
DCota <32775237+DaigaroCota@users.noreply.github.com>
2023-10-26 21:07:32 -0700

    refactor: deployment scripts for diamond and price bulletin

--- scripts/hardhat/deployPriceBulletin.js
+++ scripts/hardhat/deployPriceBulletin.js
@@ -1,41 +1,41 @@
 /* global ethers */
 const { ethers } = require("hardhat")
+const { setPublisherPriceBulletin } = require("./setPublisherPriceBulletin")
 
 const DEBUG = false
 
-async function deployPriceBulletin() {
-  const accounts = await ethers.getSigners()
-  const contractOwner = accounts[0]
-
+async function deployPriceBulletin(diamondAddress) {
   const PriceBulletin = await ethers.getContractFactory("PriceBulletin")
   const pricebulletin = await PriceBulletin.deploy()
   await pricebulletin.deployed()
 
   const ERC1967Proxy = await ethers.getContractFactory("ERC1967Proxy")
-  let data = (await pricebulletin.populateTransaction.initialize()).data
+  let data = (await pricebulletin.populateTransaction.initialize(diamondAddress)).data
   const proxy = await ERC1967Proxy.deploy(pricebulletin.address, data)
-
-  let calldata = (await pricebulletin.populateTransaction.setAuthorizedPublisher('0x2a895CF57ef296d1C63FE082053238D99D749774', true)).data
-  let utx = {
-    to: proxy.address,
-    data: calldata
-  }
-
-  const tx = await contractOwner.sendTransaction(utx);
-  await tx.wait()
-
+  
   if (DEBUG) {
     console.log()
     console.log('PriceBulletin implementation deployed at:', pricebulletin.address)
     console.log('PriceBulletin proxy:', proxy.address)
   }
 
+  setPublisherPriceBulletin(proxy.address, '0x2a895CF57ef296d1C63FE082053238D99D749774')
+
   return proxy.address
 }
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 if (require.main === module) {
-  deployPriceBulletin()
+  const args = process.argv.slice(2); // Extract arguments, excluding the first two elements
+
+  if (args.length !== 1) {
+    console.error("Usage: node deployPriceBulletin.js <diamondAddr>");
+    process.exit(1);
+  }
+
+  const diamondAddr = args[0];
+  
+  deployPriceBulletin(diamondAddr)
     .then(() => process.exit(0))
     .catch(error => {
       console.error(error)
