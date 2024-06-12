1affb9d381464d2d63378f9ee8407bbba60449ac
dcota <32775237+DaigaroCota@users.noreply.github.com>
2024-06-12 11:38:41 -0700

    chore: adjust times

--- scripts/pm2/pm2-ecosystem.config.js
+++ scripts/pm2/pm2-ecosystem.config.js
@@ -6,42 +6,43 @@ const PYTH_ADDRESS_BASE = "0x8250f4aF4B972684F7b336503E2D6dFeDeB1487a";
 const PYTH_ADDRESS_OPT = "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C";
 
 const PK = process.env.PRIVATE_KEY;
+const OPTIMISM_RPC = process.env.OPTIMISM_RPC;
 
 module.exports = {
   apps: [
     {
       name: "arb-pyth-pusher",
-      script: "./pyth-pusher.js",
+      script: "./scripts/pm2/pyth-pusher.js",
       env: {
         LOCAL_CHAIN_ID: 42161,
         LOCAL_PYTH: PYTH_ADDRESS_ARB,
         LOCAL_RPC: "https://arbitrum.llamarpc.com",
       },
-      cron_restart: "0 22 * * 1-5", // This will restart the script every weekday at 6:00 AM
+      cron_restart: "0 14 * * 1-5", // This will restart the script every weekday at 2:00 PM
       cron_timezone: "America/Los_Angeles", // This will set the timezone to "America/Los_Angeles"
       autorestart: false, // This prevents PM2 from automatically restarting your script if it crashes or stops
     },
     {
       name: "base-pyth-pusher",
-      script: "./pyth-pusher.js",
+      script: "./scripts/pm2/pyth-pusher.js",
       env: {
         LOCAL_CHAIN_ID: 8453,
         LOCAL_PYTH: PYTH_ADDRESS_BASE,
         LOCAL_RPC: "https://mainnet.base.org",
       },
-      cron_restart: "0 22 * * 1-5", // This will restart the script every weekday at 6:00 AM
+      cron_restart: "0 7,14 * * 1-5", // This will restart the script every weekday at 7:00 AM and 2:00 PM
       cron_timezone: "America/Los_Angeles", // This will set the timezone to "America/Los_Angeles"
       autorestart: false, // This prevents PM2 from automatically restarting your script if it crashes or stops
     },
     {
       name: "optimism-pyth-pusher",
-      script: "./pyth-pusher.js",
+      script: "./scripts/pm2/pyth-pusher.js",
       env: {
         LOCAL_CHAIN_ID: 10,
         LOCAL_PYTH: PYTH_ADDRESS_OPT,
-        LOCAL_RPC: "https://optimism.drpc.org",
+        LOCAL_RPC: OPTIMISM_RPC,
       },
-      cron_restart: "0 22 * * 1-5", // This will restart the script every weekday at 6:00 AM
+      cron_restart: "0 14 * * 1-5", // This will restart the script every weekday at 2:00 PM
       cron_timezone: "America/Los_Angeles", // This will set the timezone to "America/Los_Angeles"
       autorestart: false, // This prevents PM2 from automatically restarting your script if it crashes or stops
     },
